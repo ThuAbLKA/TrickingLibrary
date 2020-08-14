49cfe71e6f81c1f0b57de455bf70e48993595dc2
ThuAbLKA <thusith.92@gmail.com>
2020-08-14 14:34:03 +1000

    - added todo items

---
+++ TrickingLibrary.Api/Generics/GenericService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrickingLibrary.Api.Generics
+{
+   public class GenericService<T>
+   {
+   }
+}
---
+++ TrickingLibrary.Api/Repositories/TrickRepository.cs
@@ -0,0 +1,13 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TrickingLibrary.Api.Models;
+
+namespace TrickingLibrary.Api.Repositories
+{
+   public class TrickRepository
+   {
+   }
+}
